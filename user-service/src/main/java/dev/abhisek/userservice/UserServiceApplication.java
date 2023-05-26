c4ddb5a16dfa182511188faec2dfd59d9e666b02
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-26 13:52:48 +0530

    annotated with a feign client

--- user-service/src/main/java/dev/abhisek/userservice/UserServiceApplication.java
+++ user-service/src/main/java/dev/abhisek/userservice/UserServiceApplication.java
@@ -2,12 +2,14 @@ package dev.abhisek.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
 }
