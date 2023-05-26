f1df4f514386a46f12abec013e1424d0c6f3f58e
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-26 13:56:24 +0530

    dao layer

---
+++ user-service/src/main/java/dev/abhisek/userservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.abhisek.userservice.repository;
+
+import dev.abhisek.userservice.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
