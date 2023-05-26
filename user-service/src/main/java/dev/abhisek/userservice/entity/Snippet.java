23b3b1a59627cec8accaf4145e11741e08cf732e
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-26 13:55:15 +0530

    entity for snippets

---
+++ user-service/src/main/java/dev/abhisek/userservice/entity/Snippet.java
@@ -0,0 +1,13 @@
+package dev.abhisek.userservice.entity;
+
+import lombok.Data;
+
+@Data
+public class Snippet {
+
+    private String snippetId;
+    private String title;
+    private String description;
+    private String code;
+    private String userId;
+}
