17015afcf6e4b1c4ec4cf97cff8d34dc4ade8242
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-26 13:54:59 +0530

    created snippet service

---
+++ snippet-service/src/main/java/dev/abhisek/snippetservice/SnippetServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.abhisek.snippetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetServiceApplication.class, args);
+	}
+
+}
---
+++ snippet-service/src/test/java/dev/abhisek/snippetservice/SnippetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abhisek.snippetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
