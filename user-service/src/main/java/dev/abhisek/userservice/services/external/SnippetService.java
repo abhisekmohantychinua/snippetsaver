a6a512800a8df0a2d5d48e4cc3476574df8a2350
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-27 20:39:33 +0530

    feign client interface

--- user-service/src/main/java/dev/abhisek/userservice/services/external/SnippetService.java
+++ user-service/src/main/java/dev/abhisek/userservice/services/external/SnippetService.java
@@ -5,8 +5,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @FeignClient(name = "SNIPPET-SERVICE")
 public interface SnippetService {
     @GetMapping("/snippet/{snippetId}")
     Snippet getSnippetBySnippetId(@PathVariable String snippetId);
+
+    @GetMapping("/snippet/user/{userId}")
+    List<Snippet> getSnippetByUserId(@PathVariable String userId);
 }
