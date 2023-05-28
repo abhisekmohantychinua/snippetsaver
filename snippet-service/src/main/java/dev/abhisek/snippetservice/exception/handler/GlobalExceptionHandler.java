1704f797dd5790cd7929c0174ad53ada32933a1f
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-28 12:19:48 +0530

    added handler for user not found

--- snippet-service/src/main/java/dev/abhisek/snippetservice/exception/handler/GlobalExceptionHandler.java
+++ snippet-service/src/main/java/dev/abhisek/snippetservice/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package dev.abhisek.snippetservice.exception.handler;
 
 import dev.abhisek.snippetservice.exception.SnippetNotFoundException;
+import dev.abhisek.snippetservice.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +12,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(SnippetNotFoundException.class)
     public ResponseEntity<String> snippetNotFoundExceptionHandler(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> userNotFoundExceptionHandler(Exception e) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(e.getMessage());
