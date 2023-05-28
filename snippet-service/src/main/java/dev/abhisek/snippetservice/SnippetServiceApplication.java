4af3175613f8acd2af40519e7eb0ffbffaa4c6b9
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-28 12:20:44 +0530

    enabled feign client

--- snippet-service/src/main/java/dev/abhisek/snippetservice/SnippetServiceApplication.java
+++ snippet-service/src/main/java/dev/abhisek/snippetservice/SnippetServiceApplication.java
@@ -2,8 +2,10 @@ package dev.abhisek.snippetservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SnippetServiceApplication {
 
 	public static void main(String[] args) {
