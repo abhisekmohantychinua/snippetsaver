9f4e081df1dccac932607193a132049cfda632ae
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-31 07:28:55 +0530

    security configuration

---
+++ auth-service/src/main/java/dev/abhisek/authservice/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package dev.abhisek.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("auth/**")
+                .permitAll();
+
+
+        return http.build();
+    }
+}
