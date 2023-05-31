3e7de1aa481b888ea6ccc8e35407b631bbad4c0b
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-31 07:17:37 +0530

    Created rest template bean inside

--- api-gateway/src/main/java/dev/abhisek/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/dev/abhisek/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,19 @@ package dev.abhisek.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
