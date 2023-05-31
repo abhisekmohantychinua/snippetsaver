f079bc5e7e3dab783684f3faa0483f0fc155ca5d
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-05-31 07:29:20 +0530

    user repository

---
+++ auth-service/src/main/java/dev/abhisek/authservice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.abhisek.authservice.repository;
+
+import dev.abhisek.authservice.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
