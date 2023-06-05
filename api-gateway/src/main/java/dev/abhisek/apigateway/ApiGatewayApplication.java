35f927f8ec325257fb28c02db4a2ba49b54a9668
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-05 21:56:36 +0530

    main app with web client bean

--- api-gateway/src/main/java/dev/abhisek/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/dev/abhisek/apigateway/ApiGatewayApplication.java
@@ -3,7 +3,7 @@ package dev.abhisek.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -13,8 +13,8 @@ public class ApiGatewayApplication {
     }
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    WebClient webClient() {
+        return WebClient.create();
     }
 
 }
